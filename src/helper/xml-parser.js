866ae5b93e2c2a66c07154e95138aef3d21bf1c1
raoul2000 <manu34@free.fr>
2016-10-09 18:21:35 +0200

    initialize project

---
+++ src/helper/xml-parser.js
@@ -0,0 +1,158 @@
+"use strict";
+
+var DOMParser      = require('xmldom').DOMParser;
+
+/**
+ * Replace XML entities with their values based on the dictionary hash. When an entity is not
+ * found in dictionary, the errorHandler function is called with the name of the unresolved entity as
+ * argument.
+ *
+ * Dictionary hash Example :
+ * {
+ * 		'ENTITY_1' : 'value entity1',
+ * 		'ENTITY_2' : 'another value'
+ * }
+ */
+function entityResolver (str, dictionary, errorHandler){
+	var reEntity = /&([0-9a-zA-Z_]+);/g;
+	var defaultEntity = {
+		"quot" : "ENT_QUOT"
+	};
+	return str
+		.split('\n')
+		.map(
+			function(line) {
+				var resultLine  = line,
+				    entityValue = '',
+				    entityName  = '',
+				    match;
+
+				while ( (match = reEntity.exec(line) )) {
+					entityName  = match[1];
+					if( dictionary.hasOwnProperty(entityName) === false) {
+						entityValue = defaultEntity.hasOwnProperty(entityName) ?
+								defaultEntity[entityName]
+							: errorHandler(entityName);
+					} else {
+						entityValue = dictionary[entityName];
+					}
+
+					resultLine = resultLine.replace(
+						new RegExp('&'+entityName+';', 'g'),
+						entityValue
+					);
+				}
+				return resultLine;
+			}
+		)
+		.join("\n");
+}
+
+/**
+ * Parse an XML string that contains no entity and returns a result object
+ * @param  {string} strXML The XML string to parse
+ * @return {object}        (see description)
+ */
+function parseNoEntities(strXML) {
+  try {
+    var parseErrors = {
+      "warning" : [],
+      "error" : [],
+      "fatal" : []
+    };
+
+    var dom = new DOMParser({
+      errorHandler:{
+        warning   : function(w){ parseErrors.warning.push(w); },
+        error     : function(w){ parseErrors.error.push(w);   },
+        fatalError: function(w){ parseErrors.fatal.push(w);   }
+      }
+    }).parseFromString(strXML);
+
+    // consider both error and fatal as severe : no DOM is returned in this case
+    if(parseErrors.fatal.length !== 0 || parseErrors.error.length !== 0 ) {
+      throw new Error("".concat(
+        parseErrors.error.join("\n"),
+        parseErrors.fatal.join("\n")
+      ));
+    }
+    return {
+      "document"   : dom,
+      "success"    : true
+    };
+  } catch (e) {
+    return {
+      "document"   : null,
+      "success"    : false,
+      "error"      : e
+    };
+  }
+}
+
+/**
+ * Parse and returns a Document from the XML string passed as argument.
+ * If the XML string contains entities, they are resolved priori to parse
+ * using the "entities" hash map.
+ * In case of success, this function returns :
+ * {
+ * 	"document" : <the DOM>,
+ * 	"success"  : true
+ * }
+ * In case of error :
+ * {
+ * 	"document" : null,
+ * 	"success"  : false,
+ * 	"error"    : {}
+ * }
+ *
+ * @param  {string} strXML   the XML string to parse
+ * @param  {object} entities hash where key are entity name and value is entity value
+ * @param  {function} entityResolverErrorHandler entity resolution error handler
+ * @return {object}          the result of the parse as an object.
+ */
+function parse( strXML , entities, entityResolverErrorHandler) {
+
+    var result = {
+      "document": null,
+      "success" : true
+    };
+
+    if( entities ) {
+      var missingEntities = {}; // use only by private error handler
+
+      // prepare Entity Resolver Error Handler
+      var entErrorHandler, isPrivateErrorHandler;
+      if(typeof entityResolverErrorHandler === "function" ) {
+        isPrivateErrorHandler = false;
+        entErrorHandler = entityResolverErrorHandler;
+      } else {
+        isPrivateErrorHandler = true;
+        entErrorHandler = function(missingEntityName){
+          missingEntities[missingEntityName] = true;
+        };
+      }
+
+      // Performing entity resolution. If a custom error handkler has been
+      // provided, it may throw an exception to stop the parse process.
+      //
+      try {
+        strXML = entityResolver(strXML, entities, entErrorHandler);
+      } catch (e) {
+        result.success = false;
+        result.error = e;
+      }
+
+      if( isPrivateErrorHandler && Object.keys(missingEntities).length !== 0) {
+        // there are unresolved entities !!
+        result.success = false;
+        result.error = { "message" : "missing entities : " + Object.keys(missingEntities).join(',') };
+      }
+    }
+
+    if(result.success === true) {
+      result = parseNoEntities(strXML);
+    }
+    return result;
+}
+
+exports.parse = parse;
---
+++ test/test-xml-parser.js
@@ -0,0 +1,87 @@
+"use strict";
+
+var xmlParser = require('../src/helper/xml-parser'),
+		assert = require('chai').assert;
+
+describe('XML Parser',function(done){
+
+	it('resolve entities and parses an XML ',function(done){
+
+		var result = xmlParser.parse(
+			"<doc>" +
+				"<Context path=\"&ENTITY_1;\" docBase=\"docBaseValue_1\">\n</Context>"+
+				"<Context >&ENTITY_2;</Context>"+
+			"</doc>",
+			{
+				"ENTITY_1" : "value1",
+				"ENTITY_2" : "value2"
+			}
+		);
+
+		assert.isNotNull(result);
+		assert.isObject(result);
+		assert.isNotNull(result.document);
+		assert.isObject(result.document);
+		assert.isTrue(result.success);
+		done();
+	});
+
+	it('returns error if there is unresolved entities',function(done){
+
+		var result = xmlParser.parse(
+			"<doc>" +
+				"<Context path=\"&ENTITY_1;\" docBase=\"docBaseValue_1\">\n</Context>"+
+				"<Context >&ENTITY_2;</Context>"+
+			"</doc>",
+			{
+				"ENTITY_1" : "value1"
+			}
+		);
+
+		assert.isNotNull(result);
+		assert.isObject(result);
+		assert.isFalse(result.success);
+		assert.isNull(result.document);
+		assert.isObject(result.error);
+		assert.propertyVal(result.error, 'message', 'missing entities : ENTITY_2');
+		done();
+	});
+
+	it('unresolved entities are ignored by a user provided error handler',function(done){
+
+		var result = xmlParser.parse(
+			"<doc>" +
+				"<Context path=\"&ENTITY_1;\" docBase=\"docBaseValue_1\">\n</Context>"+
+				"<Context >&ENTITY_2;</Context>"+
+			"</doc>",
+			{
+				"ENTITY_1" : "value1"
+			},
+			function(entityName){
+				// ignore any unresolved entities
+			}
+		);
+
+		assert.isNotNull(result);
+		assert.isObject(result);
+		assert.isTrue(result.success);
+		assert.isNotNull(result.document);
+		done();
+	});
+
+	it('returns error if the XML is not well former',function(done){
+
+		var result = xmlParser.parse(
+			"<doc>" +
+				"Context >value</Context>"+
+			"</doc>"
+		);
+
+		assert.isNotNull(result);
+		assert.isObject(result);
+		assert.isFalse(result.success);
+		assert.isNull(result.document);
+		assert.isNotNull(result.error);
+		done();
+	});
+});
