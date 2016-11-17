8b8094e9b6ff86c4ec06faf3202671ff901f2c09
raoul2000 <manu34@free.fr>
2016-11-17 18:37:46 +0100

    add test-config

---
+++ test/test-config.js
@@ -0,0 +1,63 @@
+"use strict";
+
+var config  	= require('../src/tomcat/config'),
+	  xmlParser = require('../src/helper/xml-parser'),
+		fs 			  = require('fs'),
+	  assert  	= require('chai').assert;
+
+var cfg = {};
+var xmlEntities = {
+	"HOME" : "/home/folder"
+};
+
+describe('Tomcat config',function(done){
+
+	this.timeout(10000);
+
+	before(function() {
+    cfg = JSON.parse(fs.readFileSync(__dirname + "/config.json", "utf-8" ));
+  });
+
+	it('load tomcat config',function(done){
+
+		return config.getDOM(cfg.sshConnection, cfg.home + "/tomcat-1", xmlEntities)
+		.then(function(domConfig){
+			assert.isObject(domConfig);
+			var port = config.getPortNumber(domConfig);
+			assert.equal(port, 111);
+			done();
+		})
+		.done(null,function(err){
+			done(err);
+		});
+	});
+
+	it('fails to load tomcat config for invalid folder',function(done){
+
+		return config.getDOM(cfg.sshConnection, cfg.home + "/tomcat-NOT_FOUND", xmlEntities)
+		.then(function(domConfig){
+			done(new Error("tomcat install folder invalid"));
+		})
+		.fail(function(err){
+			done();
+		})
+		.done(null,function(err){
+			done(err);
+		});
+	});
+
+	it('fails to load tomcat config with undefined entity',function(done){
+
+		return config.getDOM(cfg.sshConnection, cfg.home + "/tomcat-1", {})
+		.then(function(domConfig){
+			done(new Error("entity missing"));
+		})
+		.fail(function(err){
+			done();
+		})
+		.done(null,function(err){
+			done(err);
+		});
+	});
+
+});
